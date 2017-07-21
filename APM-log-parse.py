8e7c878db3f6f7701bc01ac3ac56a266a92bd3d8
Samuel Vanderwaal <samuel.vanderwaal@gmail.com>
2017-07-21 13:12:28 -0800

    Code improvement: use global_pos for all values; fix argv handling error

--- APM-log-parse.py
+++ APM-log-parse.py
@@ -5,58 +5,61 @@
 
 from sys import argv
 
-# Accepts batches of files by running the command: 'python APM-log-parse.py *.csv'
-for File in argv:
-	filename = File
-	
-	coordArray = []
+# Accepts batches of files by running the command:
+# 'python APM-log-parse.py *.csv'
+for File in argv[1:]:
+    filename = File
 
-	# Define global variables for tracking values
-	global timestamp
-	global latitude
-	global longitude
-	global altitude
+    coordArray = []
 
-	# Read input file and create output file
-	inFile = open(filename)
+    # Define global variables for tracking values
+    timestamp = 0
+    latitude = 0
+    longitude = 0
+    altitude = 0
 
-	# Create an output file with the same name as input but with "CONVERTED" added at the end
-	outFileName = filename[:-4] + '-CONVERTED' + filename[-4:]
+    # Read input file and create output file
+    inFile = open(filename)
 
-	# Open output file for writing
-	outFile = open(outFileName,'w')
+    # Create an output file with the same name as input but with "CONVERTED"
+    # added at the end
+    outFileName = filename[:-4] + '-CONVERTED' + filename[-4:]
 
-	# Parse every line in the input file
-	for line in inFile:
-		# Split out the comma separated values into a Python list
-		tempList = line.split(',')
-		
-		# Check for 'lat' value
-		if 'lat' in tempList:
-			timestamp = tempList[0]							# Timestamp is always first value in a line
-			j = tempList.index('lat')						# Get index of 'lat' marker
-			variable = tempList[j+1]						# Latitude value is next value after 'lat' marker
-			latitude = variable[:2] + '.' + variable[2:]	# Add decimal point to latitude value
+    # Open output file for writing
+    outFile = open(outFileName, 'w')
 
-		# Check for 'lon' value
-		if 'lon' in tempList:
-			k = tempList.index('lon')						# Get index of 'lon' marker
-			variable1 = tempList[k+1]						# Longtidue value is next value after 'lon' marker
-			longitude = variable1[:4] + '.' + variable1[4:]	# Add decimal point to longitude value
+    # Parse every line in the input file
+    for line in inFile:
+        # Split out the comma separated values into a Python list
+        tempList = line.split(',')
 
-		# Barometric corrected altitude is in a different line than the lat and lon values
-		# Look for 'mavlink_vfr_hud_t' line marker
-		# Altitude value is six places away from marker
-		if 'mavlink_vfr_hud_t' in tempList:
-			l = tempList.index('mavlink_vfr_hud_t')
-			altitude = tempList[l+6]
+        if "mavlink_global_position_int_t" in tempList:
+            # Timestamp is always first value in a line
+            timestamp = tempList[0]
+            # Get index of 'lat' marker
+            j = tempList.index('lat')
+            # Latitude value is next value after 'lat' marker
+            lat_var = tempList[j+1]
+            # Add decimal point to latitude value
+            latitude = lat_var[:2] + '.' + lat_var[2:]
 
-			# Now that we have all our values in global variables write them to the output file
-			outFile.write(timestamp + ',' + latitude + ',' + longitude + ',' + altitude + '\n')
-	
-	# Close files		
-	inFile.close()
-	outFile.close()
+            # Get index of 'lon' marker
+            k = tempList.index('lon')
+            # Longitude value is next value after 'lon' marker
+            lon_var = tempList[k+1]
+            # Add decimal point to longitude value
+            longitude = lon_var[:4] + '.' + lon_var[4:]
 
+            # Get index of 'relative_alt' marker
+            l = tempList.index('relative_alt')
+            # Relative altitude value is next value after marker
+            # Expressed as mm; convert to meters
+            relative_alt = float(tempList[l+1]) / 1000.0
 
+            # Write values everytime we get a "global_position" packet
+            outFile.write(timestamp + ',' + latitude + ',' +
+                          longitude + ',' + str(relative_alt) + '\n')
 
+    # Close files
+    inFile.close()
+    outFile.close()
