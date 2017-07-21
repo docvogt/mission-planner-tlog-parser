c74be83caf25095763ae5995650a0a16a1dfd0fd
Samuel Vanderwaal <samuel.vanderwaal@gmail.com>
2017-07-21 14:01:37 -0800

    added GPGGA output file for use with Exiftool geotaggin

--- APM-log-parse.py
+++ APM-log-parse.py
@@ -3,6 +3,7 @@
 # Samuel Vanderwaal, 2015
 # Northern Embedded Solutions
 
+import csv
 from sys import argv
 
 # Accepts batches of files by running the command:
@@ -17,6 +18,9 @@ for File in argv[1:]:
     latitude = 0
     longitude = 0
     altitude = 0
+    latRef = 'N/A'
+    lonRef = 'N/A'
+    rel_alt = '0'
 
     # Read input file and create output file
     inFile = open(filename)
@@ -25,41 +29,88 @@ for File in argv[1:]:
     # added at the end
     outFileName = filename[:-4] + '-CONVERTED' + filename[-4:]
 
-    # Open output file for writing
-    outFile = open(outFileName, 'w')
+    # Open output files for writing
 
-    # Parse every line in the input file
-    for line in inFile:
-        # Split out the comma separated values into a Python list
-        tempList = line.split(',')
+    with open(outFileName, 'w') as csvFile,\
+            open(outFileName[0:-4] + '_GPGGA' + '.csv', 'w') as gpggaFile:
 
-        if "mavlink_global_position_int_t" in tempList:
-            # Timestamp is always first value in a line
-            timestamp = tempList[0]
-            # Get index of 'lat' marker
-            j = tempList.index('lat')
-            # Latitude value is next value after 'lat' marker
-            lat_var = tempList[j+1]
-            # Add decimal point to latitude value
-            latitude = lat_var[:2] + '.' + lat_var[2:]
+        csvwriter = csv.writer(csvFile, delimiter=',')
+        gpggawriter = csv.writer(gpggaFile, delimiter=',')
 
-            # Get index of 'lon' marker
-            k = tempList.index('lon')
-            # Longitude value is next value after 'lon' marker
-            lon_var = tempList[k+1]
-            # Add decimal point to longitude value
-            longitude = lon_var[:4] + '.' + lon_var[4:]
+        # Parse every line in the input file
+        for line in inFile:
+            # Split out the comma separated values into a Python list
+            tempList = line.split(',')
 
-            # Get index of 'relative_alt' marker
-            l = tempList.index('relative_alt')
-            # Relative altitude value is next value after marker
-            # Expressed as mm; convert to meters
-            relative_alt = float(tempList[l+1]) / 1000.0
+            if "mavlink_global_position_int_t" in tempList:
+                # Timestamp is always first value in a line
+                timestamp = tempList[0]
+                # Get index of 'lat' marker
+                j = tempList.index('lat')
+                # Latitude value is next value after 'lat' marker
+                lat_var = tempList[j+1]
+                # Add decimal point to latitude value
+                latitude = lat_var[:2] + '.' + lat_var[2:]
 
-            # Write values everytime we get a "global_position" packet
-            outFile.write(timestamp + ',' + latitude + ',' +
-                          longitude + ',' + str(relative_alt) + '\n')
+                if latitude[0:1] == '-':
+                    latRef = 'S'
+                else:
+                    latRef = 'N'
 
-    # Close files
+                # Get index of 'lon' marker
+                k = tempList.index('lon')
+                # Longitude value is next value after 'lon' marker
+                lon_var = tempList[k+1]
+                # Add decimal point to longitude value
+                longitude = lon_var[:4] + '.' + lon_var[4:]
+
+                if longitude[0:1] == '-':
+                    lonRef = 'W'
+                else:
+                    lonRef = 'E'
+
+                # Get index of 'relative_alt' marker
+                l = tempList.index('relative_alt')
+                # Relative altitude value is next value after marker
+                # Expressed as mm; convert to meters
+                relative_alt = float(tempList[l+1]) / 1000.0
+                rel_alt = str(relative_alt)
+
+                # Write values everytime we get a "global_position" packet
+                csvwriter.writerow([timestamp, latitude, longitude, rel_alt])
+
+            if "mavlink_gps_raw_int_t" in tempList:
+                # Get only the time, not date
+                time = tempList[0].split('T')[1]
+
+                # Get index of 'fix_type' marker
+                m = tempList.index('fix_type')
+                fix_type = int(tempList[m+1])
+
+                # Mavlink fix is invalid (0-1), 2D (2), and 3D (3)
+                # GPGGA fix quality is invalid (0), GPS (1) and RTK (2)
+                # We assume only GPS or invalid values
+                if fix_type > 1:
+                    fixQ = 1
+                else:
+                    fixQ = 0
+
+                n = tempList.index('satellites_visible')
+                nSats = tempList[n+1]
+
+                o = tempList.index('eph')
+                HDOP = float(tempList[o+1])/100.0
+
+                # No way currently to get geoid height above WGS84 ellipsoid
+                height = ''
+
+                # No need for actual chksum
+                chksum = ''
+
+                # Create GPGGA file to allow geotagging with exiftool
+                gpggawriter.writerow(["$GPGGA", time, latitude, latRef,
+                                     longitude, lonRef, fixQ, nSats, HDOP,
+                                     rel_alt, 'M', height, 'M', chksum])
+
+    # Close file
     inFile.close()
-    outFile.close()
